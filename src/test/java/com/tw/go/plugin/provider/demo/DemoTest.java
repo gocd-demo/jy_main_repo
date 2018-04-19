c5efeb3f352f74ef9771c67a6e18f472665c1bde
Ken Mugrage <kmugrage@users.noreply.github.com>
2018-04-19 15:02:10 -0700

    Added demo test fail

---
+++ src/test/java/com/tw/go/plugin/provider/demo/DemoTest.java
@@ -0,0 +1,13 @@
+package com.tw.go.plugin.provider.github;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DemoTest {
+
+    @Test
+    public void compareNumbers() {
+        Assert.assertEquals(4, 5);
+    }
+
+}
